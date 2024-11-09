fbd6a1d1ffb3dfa5eb641882a255d3284ea0cfa4
Siddharth-M-77 <siddharthmaddheshiya777@gmail.com>
2024-11-09 16:38:42 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        'module-name' // Replace 'module-name' with the actual module that Vite suggests externalizing
+      ]
+    }
+  }
 })
