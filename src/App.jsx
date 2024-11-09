8893b34c6e85b28826fdfabbcf07d7d973d26800
Siddharth-M-77 <siddharthmaddheshiya777@gmail.com>
2024-11-09 12:52:29 +0530

    setup-complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import Home from './components/home'
+import React from 'react';
+import Home from './components/Home'; // Adjust the path if needed
 
 const App = () => {
   return (
     <div>
-      <Home/>
+      <Home />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const Home = () => {
   return (
     <div>
       <h1 className='text-center text-8xl text-indigo-500'>Heyyyy</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
