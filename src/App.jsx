5f9ae39e4bd8f426a162f5a90dadec03b9521717
Siddharth-M-77 <siddharthmaddheshiya777@gmail.com>
2024-11-09 16:34:12 +0530

    Topnav-complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Home from "./components/Home"; // Adjust the path if needed
+import Home from "./components/Home";
 import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="min-w-screen-2xl min-h-screen bg-[#1F1E24]">
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </div>
   );
